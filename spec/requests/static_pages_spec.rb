43364bfc57b2c544fe7cf6e8758d4a81f1e1e72a
dipeshingle12345 <mfsi.dipeshi@gmail.com>
2023-04-03 20:13:24 +0530

    layout links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 
 
 describe "Static pages" do
+  subject { page }
   include Capybara::DSL
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
@@ -41,27 +42,18 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
+    describe "Contact page" do
+      before { visit about_path }
+      it { should have_content('About Us') }
+  
+     it { should have_title("#{base_title} | About Us") }
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
+    it { should have_content('Contact Us') }
 
-    it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact Us')
-    end
-
-    it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact Us")
-    end
+   it { should have_title("#{base_title} | Contact Us") }
   end
 end
