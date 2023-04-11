4a81e1cbcf273cc3495c4e52f503990bd86c5fb4
dipeshingle12345 <mfsi.dipeshi@gmail.com>
2023-04-11 19:00:04 +0530

    signin success

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # get 'users/new'
 
@@ -13,6 +14,8 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
   match '/signup',  to: 'users#new',    via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
    
 
